b7ec8fbc8e93c6b1d6febdd2a066a2d682e9bc13
Ernie Dotson <18388563+erniedotson@users.noreply.github.com>
2022-05-14 20:15:52 +0000

    Add .bin/install.sh

---
+++ .bin/install.sh
@@ -0,0 +1,17 @@
+myrepo=https://github.com/erniedotson/dotfiles.git
+mydir=.dotfiles
+
+git clone --bare "${myrepo}" "${HOME}"/${mydir}
+function dfgit {
+   /usr/bin/git --git-dir=${HOME}/.cfg/ --work-tree=${HOME} $@
+}
+mkdir -p .dotfiles-backup
+dfgit checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dotfiles.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dfgit checkout
+# dfgit config status.showUntrackedFiles no
