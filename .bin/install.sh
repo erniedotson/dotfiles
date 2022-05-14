63e7d97b86376be577511c481203393d25cb6e2b
Ernie Dotson <18388563+erniedotson@users.noreply.github.com>
2022-05-14 21:05:49 +0000

    Fix .bin/install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -5,13 +5,13 @@ git clone --bare "${myrepo}" "${HOME}"/${mydir}
 function dfgit {
    /usr/bin/git --git-dir=${HOME}/${mydir}/ --work-tree=${HOME} $@
 }
-mkdir -p .dotfiles-backup
+mkdir -p ~/.dotfiles-backup
 dfgit checkout
 if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
   else
     echo "Backing up pre-existing dotfiles.";
-    dfgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dfgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} ~/.dotfiles-backup/{}
 fi;
 dfgit checkout
 # dfgit config status.showUntrackedFiles no
