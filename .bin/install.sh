c8bdad6b336e247263b9852a2dd53a4e5a9f3d7f
Ernie Dotson <18388563+erniedotson@users.noreply.github.com>
2022-05-14 21:04:06 +0000

    Fix .bin/install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -11,7 +11,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
   else
     echo "Backing up pre-existing dotfiles.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dfgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 dfgit checkout
 # dfgit config status.showUntrackedFiles no
