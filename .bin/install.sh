43e61054eec5103721eba68c7429244a4818270d
Ernie Dotson <18388563+erniedotson@users.noreply.github.com>
2022-05-14 21:31:39 +0000

    Fix .bin/install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,10 +1,13 @@
-myrepo=git@github.com:erniedotson/dotfiles.git
+myrepo_https=https://github.com/erniedotson/dotfiles.git
+# myrepo_ssh=git@github.com:erniedotson/dotfiles.git
 mydir=.dotfiles
 
-git clone --bare "${myrepo}" "${HOME}/${mydir}"
+# Clone using https so we don't require an ssh key to be registered
+git clone --bare "${myrepo_https}" "${HOME}/${mydir}"
 function dfgit {
    /usr/bin/git --git-dir="${HOME}/${mydir}/" --work-tree="${HOME}" $@
 }
+
 mkdir -p ~/.dotfiles-backup
 dfgit checkout
 if [ $? = 0 ]; then
