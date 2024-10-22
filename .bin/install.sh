80973b41489834c6fd467f6a1f7e1ab67f12583a
Ernie Dotson <18388563+erniedotson@users.noreply.github.com>
2024-10-22 16:39:59 -0400

    refactor(install.sh): Added better error handling and support for --git-dir
    Handle failures better, fix backups to handle directories, handle both environments that require --git-dir to point to the .git directory and those that require it to point to the git repo (but not the .git directory).

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,46 +1,70 @@
+#!/bin/bash
+set -uo pipefail
+IFS=$'\n\t'
+
 myrepo_https=https://github.com/erniedotson/dotfiles.git
 myrepo_ssh=git@github.com:erniedotson/dotfiles.git
 mydir=.dotfiles
-
-# Alias, as it should appear in the file:
-#   alias dfgit='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-# NOTE: Careful with escape characters in strings below.
-myaliasstring="alias dfgit='git --git-dir=\$HOME/.dotfiles/ --work-tree=\$HOME'"
+need_git_dir=false
 
 function dfgit {
-   git --git-dir="${HOME}/${mydir}/" --work-tree="${HOME}" $@
+  if [[ "${need_git_dir}" = true ]]; then
+    git --git-dir="${HOME}/${mydir}/.git" --work-tree="${HOME}" $@
+  else
+    git --git-dir="${HOME}/${mydir}" --work-tree="${HOME}" $@
+  fi
 }
 
 # Using https so we can clone and pull anoynymously
-git clone --bare "${myrepo_https}" "${HOME}/${mydir}"
+printf "\nCloning repo...\n"
+git clone --bare "${myrepo_https}" "${HOME}/${mydir}" || exit 1
+
+# Test if we need to include the .git dir in --git-dir.
+git --git-dir="${HOME}/${mydir}/.git" --work-tree="${HOME}" status > /dev/null 2>&1 && need_git_dir=true || need_git_dir=false
 
 # Add SSH remote, set to default push, in case we want to push changes later.
 # This will require an SSH public key to be registered with GH.
-dfgit remote add origin-ssh ${myrepo_ssh} 
-dfgit config remote.pushdefault origin-ssh
+printf "\nConfiguring repo...\n"
+dfgit remote add origin-ssh ${myrepo_ssh} || exit 1
+dfgit config remote.pushdefault origin-ssh || exit 1
 
 # Checkout files, making an attempt to backup conflicted files
+printf "\nChecking out files...\n"
 mkdir -p ~/.dotfiles-backup
-dfgit checkout
-if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
-  else
-    echo "Backing up pre-existing dotfiles.";
-    dfgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} ~/.dotfiles-backup/{}
+
+if dfgit checkout ; then
+  printf "\nChecked out dotfiles.\n";
+else
+  printf "\nCheckout failed due to conflicts. Backing up pre-existing dotfiles...\n";
+  dfgit checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs sh -c 'for arg do echo "Moving $arg..."; mkdir -p ~/.dotfiles-backup/$(dirname $arg); mv ~/$arg ~/.dotfiles-backup/$(dirname $arg)/$(basename $arg); done' _
+  printf "\nChecking out files once more...\n"
+  dfgit checkout || exit 1
 fi;
-dfgit checkout
 
 # Hide untracked files from 'git status'
 # dfgit config status.showUntrackedFiles no
 
 # Update profile files
+
+if [[ "${need_git_dir}" = true ]]; then
+  # Alias, as it should appear in the file:
+  #   alias dfgit='git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME'
+  # NOTE: Careful with escape characters in strings below.
+  myaliasstring="alias dfgit='git --git-dir=\$HOME/.dotfiles/.git --work-tree=\$HOME'"
+else
+  # Alias, as it should appear in the file:
+  #   alias dfgit='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+  # NOTE: Careful with escape characters in strings below.
+  myaliasstring="alias dfgit='git --git-dir=\$HOME/.dotfiles --work-tree=\$HOME'"
+fi
+
 if [ -f ~/.bashrc ]; then
-    echo "Adding alias to ~/.bashrc"
-    grep -qxF "${myaliasstring}" ~/.bashrc || echo "${myaliasstring}" >> ~/.bashrc
+  printf "\nAdding alias to ~/.bashrc ...\n"
+  grep -qxF "${myaliasstring}" ~/.bashrc || echo "${myaliasstring}" >> ~/.bashrc
 fi
 if [ -f ~/.zshrc ]; then
-    echo "Adding alias to ~/.zshrc"
-    grep -qxF "${myaliasstring}" ~/.zshrc || echo "${myaliasstring}" >> ~/.zshrc
+  printf "\nAdding alias to ~/.zshrc ...\n"
+  grep -qxF "${myaliasstring}" ~/.zshrc || echo "${myaliasstring}" >> ~/.zshrc
 fi
-echo "Add the following alias to other shell profile scripts:"
-echo "  alias dfgit='git --git-dir=\$HOME/.dotfiles/ --work-tree=\$HOME'"
+printf "\nOptional: Add the following alias to other shell profile scripts:\n"
+printf "  ${myaliasstring}\n\n"
